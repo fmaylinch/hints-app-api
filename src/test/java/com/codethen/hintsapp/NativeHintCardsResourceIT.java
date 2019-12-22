bcc98542a8508efe9973088ac915db7a35900e9b
fmaylinch <ferranmaylinch@gmail.com>
2019-12-22 18:50:44 +0100

    Create Quarkus project

---
+++ src/test/java/com/codethen/hintsapp/NativeHintCardsResourceIT.java
@@ -0,0 +1,9 @@
+package com.codethen.hintsapp;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHintCardsResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
