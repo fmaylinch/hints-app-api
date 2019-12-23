945d20c38efbc17f120bf3082fb84157966f7480
fmaylinch <ferranmaylinch@gmail.com>
2019-12-23 03:11:04 +0100

    Add HintCardsResource CRUD operations

--- src/main/java/com/codethen/hintsapp/HintCardsResource.java
+++ src/main/java/com/codethen/hintsapp/HintCardsResource.java
@@ -3,31 +3,38 @@ package com.codethen.hintsapp;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
 @Path("/cards")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class HintCardsResource {
 
-    // TODO: This is done by the quarkus-mongodb-client dependency,
+    private final MongoCollection<Document> collection;
+
+    // TODO: MongoClient is provided by the quarkus-mongodb-client dependency,
     //  but maybe we should add the official mongodb dependency and configure this manually.
     //  I could also use Spring Mongo support, which does automatic mapping.
-    @Inject MongoClient mongoClient;
+    public HintCardsResource(MongoClient mongoClient) {
+        collection = mongoClient.getDatabase("hintsapp").getCollection("cards");
+    }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<HintCard> getAll() {
 
         final List<HintCard> result = new ArrayList<>();
 
-        try (MongoCursor<Document> cursor = getCollection().find().iterator()) {
+        try (MongoCursor<Document> cursor = collection.find().iterator()) {
             while (cursor.hasNext()) {
                 final Document doc = cursor.next();
                 final HintCard card = HintsCardMongoAdapter.from(doc);
@@ -37,7 +44,44 @@ public class HintCardsResource {
         }
     }
 
-    private MongoCollection<Document> getCollection(){
-        return mongoClient.getDatabase("hintsapp").getCollection("cards");
+    @GET @Path("/{id}")
+    public HintCard getOne(@PathParam String id) {
+
+        final Document doc = collection.find(byId(id)).first();
+        return HintsCardMongoAdapter.from(doc);
+    }
+
+    @DELETE @Path("/{id}")
+    public HintCard deleteOne(@PathParam String id) {
+
+        final HintCard card = getOne(id);
+        if (card != null) {
+            collection.deleteOne(byId(id));
+        }
+
+        return card;
+    }
+
+    @POST
+    public HintCard saveOrUpdate(HintCard card) {
+
+        if (card.getHints() == null || card.getHints().isEmpty())
+            throw new WebApplicationException("At least one hint is required", Response.Status.BAD_REQUEST);
+
+        final Document doc = HintsCardMongoAdapter.from(card);
+        assert doc != null;
+
+        if (card.getId() != null) {
+            collection.updateOne(byId(card.getId()), doc);
+        } else {
+            collection.insertOne(doc);
+            card.setId(doc.getObjectId("_id").toString());
+        }
+
+        return card;
+    }
+
+    private Bson byId(String id) {
+        return new Document("_id", new ObjectId(id));
     }
 }
--- src/main/java/com/codethen/hintsapp/HintsCardMongoAdapter.java
+++ src/main/java/com/codethen/hintsapp/HintsCardMongoAdapter.java
@@ -1,14 +1,29 @@
 package com.codethen.hintsapp;
 
+import com.mongodb.lang.Nullable;
 import org.bson.Document;
 
 public class HintsCardMongoAdapter {
 
-    public static HintCard from(Document doc) {
+    @Nullable
+    public static HintCard from(@Nullable Document doc) {
+
+        if (doc == null) return null;
+
         final HintCard card = new HintCard();
         card.setId( doc.getObjectId("_id").toString() );
         card.setHints( doc.getList("hints", String.class) );
         card.setNotes( doc.getString("notes") );
         return card;
+    }
+
+    @Nullable
+    public static Document from(@Nullable HintCard card) {
+
+        if (card == null) return null;
+
+        return new Document()
+                .append("hints", card.getHints())
+                .append("notes", card.getNotes());
     }
 }
