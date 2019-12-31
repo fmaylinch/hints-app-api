d4b21c3d3cec8d06f670f7949b9a8459958f92aa
fmaylinch <ferranmaylinch@gmail.com>
2019-12-31 12:09:43 +0100

    Simplify sorting by hints[0]

--- src/main/java/com/codethen/hintsapp/HintsCardMongoAdapter.java
+++ src/main/java/com/codethen/hintsapp/HintsCardMongoAdapter.java
@@ -11,7 +11,6 @@ public class HintsCardMongoAdapter {
 
     private static class Fields {
         /** Used to sort. See {@link #sortByFirstHint}. */
-        public static String hint1 = "hint1";
         public static String score = "score";
         public static String hints = "hints";
         public static String notes = "notes";
@@ -37,14 +36,12 @@ public class HintsCardMongoAdapter {
 
         return doc()
                 .append(Fields.score, card.getScore())
-                .append(Fields.hint1, card.getHints().get(0))
                 .append(Fields.hints, card.getHints())
                 .append(Fields.notes, card.getNotes());
     }
 
     public static Bson sortByFirstHint() {
-        // TODO: Maybe we can get rid of hint1 and do this: https://stackoverflow.com/a/52320364/1121497
-        //       In that case, make sure index can be created too.
-        return doc(Fields.hint1, 1);
+        // Sort by hints[0] - https://stackoverflow.com/a/52320364/1121497
+        return doc(Fields.hints + ".0", 1);
     }
 }
