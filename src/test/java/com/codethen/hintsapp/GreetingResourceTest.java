aa9411c111aa00908abb865265e620b3ef1cde00
Ferran Maylinch <ferran@yandex-team.ru>
2021-04-28 11:26:50 +0200

    Update Quarkus (now using JDK 11)

--- src/test/java/com/codethen/hintsapp/GreetingResourceTest.java
+++ src/test/java/com/codethen/hintsapp/GreetingResourceTest.java
@@ -8,14 +8,10 @@ import java.util.UUID;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-// TODO: This test failed with this error:
-//   java.lang.NoClassDefFoundError: Could not initialize class org.codehaus.groovy.vmplugin.v7.Java7
-//   Maybe because I started using JDK 15.
-
-//@QuarkusTest
+@QuarkusTest
 public class GreetingResourceTest {
 
-    //@Test
+    @Test
     public void testHelloEndpoint() {
         given()
                 .when().get("/hello")
@@ -24,7 +20,7 @@ public class GreetingResourceTest {
                 .body(is("hello"));
     }
 
-    //@Test
+    @Test
     public void testGreetingEndpoint() {
         String uuid = UUID.randomUUID().toString();
         given()
