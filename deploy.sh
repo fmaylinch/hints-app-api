2f094f436871f24d3dccb6a11089590c1ac379f1
Ferran Maylinch <ferran@yandex-team.ru>
2021-04-28 20:18:58 +0200

    Remove old script to restart AWS services

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ yc container registry configure-docker --profile fm
 echo "--- Pushing Docker image ---"
 docker push cr.yandex/crp81dg788qn7ff84jpi/hints_app_api-jvm
 
-echo "--- Pull docker image from VM and run it ---"
+echo "--- Restart Docker in VM ---"
 echo "$ ssh fmaylinch@130.193.46.10"
 echo "$ cd hint-cards"
 echo "$ ./restart-docker.sh"
